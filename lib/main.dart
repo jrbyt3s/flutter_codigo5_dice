c58398c88b1279d0a63a565e42fb01f73152227f
jrbyt3s <josee.ramirezi@gmail.com>
2022-04-22 19:38:29 -0500

    1st change, making UI

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,28 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      home: DicePage(),
+    );
+  }
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      appBar: AppBar(
+          title: Text('Dice App'),
+          centerTitle: true,
+          backgroundColor: Colors.deepPurple),
     );
   }
 }
